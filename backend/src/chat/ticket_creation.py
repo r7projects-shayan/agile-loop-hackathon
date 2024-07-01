c0986a3b24cc090e97c0a005270a803f23d62ff7
ichwan haryo sembodo <ichwanharyosembodo96@gmail.com>
2024-07-01 19:59:00 +0900

    working django server with JIRA and Gemini

--- backend/src/chat/ticket_creation.py
+++
@@ -1,9 +0,0 @@
-from src.jira.jira_api import create_ticket
-
-def handle_create_ticket(chat_input):
-    project_key = chat_input.get("project_key", "ALH")
-    summary = chat_input.get("summary", "No summary provided")
-    description = chat_input.get("description", "No description provided")
-    
-    response = create_ticket(project_key, summary, description)
-    return response
